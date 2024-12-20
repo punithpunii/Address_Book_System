b372b745d246d98ac0c0718158f83351b3ca3b10
punithpunii <punithmurthy2021@gmail.com>
2024-12-20 15:05:35 +0530

    modified added Console input

--- AddressBookMain.py
+++ AddressBookMain.py
@@ -1,7 +1,7 @@
-#UC1 create a Contacts in Address Book
-
 from contact import Contact
 from AddressBook import Addressbook
+
+#UC2 - add a new Contact to Address Book
 
 class AddressBookMain:
     def __init__(self):
@@ -12,8 +12,21 @@ class AddressBookMain:
         while True:
             choice=int(input("1. Add Contact:"))
             if choice==1:
-                contact1 = Contact("Punith", "M", "HSR", "Bangalore", "KA", "560100", "9830903020", "punith@gmail.com")
-                self.address_book.add_contact(contact1)
+                self.add_contact_console()
+
+    def add_contact_console(self):  # add a new Contact
+        print("Enter the details:")
+        firstname=input("FirstName:")
+        lastname=input("LastName:")
+        address=input("Address:")
+        city=input("City:")
+        state=input("State:")
+        zip=input("Zip:")
+        phonenumber=input("Phone_Num:")
+        email=input("E-mail:")
+
+        new_contact=Contact(firstname,lastname,address,city,state,zip,phonenumber,email)
+        self.Ab.add_contact(new_contact)         
     
         
 add1=AddressBookMain()

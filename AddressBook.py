a4d7c48f0c2fb94b3b521e801c7a3bf5457a251e
punithpunii <punithmurthy2021@gmail.com>
2024-12-20 15:04:23 +0530

    Added AddressBook

--- AddressBook.py
+++ AddressBook.py
@@ -3,7 +3,7 @@ class Addressbook:
     def __init__(self):
         self.contacts=[]
 
-    def add_contact(self,contact):
+    def add_contact(self,contact): 
         self.contacts.append(contact)
         print("Contact Added SuccessFully!",contact)
 

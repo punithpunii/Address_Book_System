0f036215320faf37999a60fba343a4f8e48410ba
punithpunii <punithmurthy2021@gmail.com>
2024-12-20 15:04:52 +0530

    Added Contacts of AddressBook

--- contact.py
+++ contact.py
@@ -9,7 +9,7 @@ class Contact:
         self.phonenumber=phonenumber
         self.email=email
 
-    def __str__(self):
+    def __str__(self):  #UC1 create a Contacts in Address Book
 
         return f"{self.firstname},{self.lastname}, {self.address}, {self.city}, {self.state}, {self.zip}, {self.phonenumber}, {self.email}"
     

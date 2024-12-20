e31c8d5aede0cab7cd3ff5760d83ee5e7e9a95c6
punithpunii <punithmurthy2021@gmail.com>
2024-12-20 15:13:53 +0530

    Added AddressBook with edit contact

--- AddressBook.py
+++ AddressBook.py
@@ -3,8 +3,12 @@ class Addressbook:
     def __init__(self):
         self.contacts=[]
 
-    def add_contact(self,contact): 
+    def add_contact(self,contact):  # add a new Contact
         self.contacts.append(contact)
         print("Contact Added SuccessFully!",contact)
 
-    
+    def edit_contact(self,firstname,updated_contact):  # edit existing contact
+        for index,contact in enumerate(self.contacts):
+            if contact.firstname == firstname:
+                self.contacts[index]=updated_contact
+                print("Contact Edited Successfully!",updated_contact)

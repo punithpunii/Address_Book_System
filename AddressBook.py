a24a0d77c5f7e088e731cd22f141e4f2b13292e2
punithpunii <punithmurthy2021@gmail.com>
2024-12-26 11:08:02 +0530

    Address Book has a unique Name

--- AddressBook.py
+++ AddressBook.py
@@ -1,27 +1,34 @@
+from contact import Contact
 class Addressbook:
 
     def __init__(self):
-        self.contacts=[]
+        self.contacts={}
 
-    def add_contact(self,contact):  # add a new Contact
-        self.contacts.append(contact)
-        print("Contact Added SuccessFully!",contact)
-
+    def add_contact(self,contact,firstname):  # add a new Contact
+        if firstname in self.contacts: # UC-7 there is no Duplicate Entry of the same Person in a particular Address Book
+                print(f"Contact with {firstname} already exists!")
+        else:
+            self.contacts[firstname]=contact
+            print("Contact Added SuccessFully!",contact)
+            
     def edit_contact(self,firstname,updated_contact):  # edit existing contact
-        for index,contact in enumerate(self.contacts):
-            if contact.firstname == firstname:
-                self.contacts[index]=updated_contact
-                print("Contact Edited Successfully!",updated_contact)
+        if firstname in self.contacts:
+            self.contacts[firstname]=updated_contact
+            print("Contact Edited Successfully!",updated_contact)
+        else:
+            print(f"Contact with {firstname} Not found!")
  
     def delete_contact(self,firstname):  # delete existing contact
-        for contact in self.contacts:
-            if contact.firstname==firstname:
-                self.contacts.remove(contact)
-                print("Contact deleted Successfully!")
-            else:
-                print("Invalid Name!")
+        if firstname in self.contacts:
+            del self.contacts[firstname]
+            print("Contact deleted Successfully!")
+        else:
+            print("Invalid Name!")
 
     def view_all_contact(self):  # view all the existing contact
         print("All Contacts:")
-        for contact in self.contacts:
-            print(contact)
+        for name,values in self.contacts.items():
+            if isinstance(values,Contact):
+                print(f"{name}: {values}")
+
+    

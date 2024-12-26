ecd47840c1c79e8f8f0a1a872a540a466ee5f1c0
punithpunii <punithmurthy2021@gmail.com>
2024-12-26 11:07:25 +0530

    modified added multiple AddressBook

--- AddressBookMain.py
+++ AddressBookMain.py
@@ -1,28 +1,72 @@
 from contact import Contact
 from AddressBook import Addressbook
 
-#UC5 - add multiple person to Address Book
+#UC6 - add multiple Address Book to the System. Each Address Book has a unique Name
 class AddressBookMain:
     def __init__(self):
-        self.address_book=Addressbook()
-        
-    def display_menu(self):
+        self.address_book_sytem={}
+
+    def display_addressbook_menu(self):
         print("Welcome to Address Book Program")
-        while True:  # add multiple person
-            choice=int(input("1. Add Contact\n2. Edit Contact\n3. Delete Contact\n4. View Contacts\nEnter your choice:"))
+        while True:
+            choice=int(input("1. Create New AddressBook\n2. Manage Existing AddressBook\n3. Delete Existing AddressBook\n4. View all the AddressBook\nEnter your choice:"))
             if choice==1:
-                self.add_contact_console()
+                name=input("Enter the name of AddressBook to be created:")
+                self.check_add_addbook(name)
+
             elif choice==2:
-                self.edit_contact_console()
+                name=input("Enter the name of AddressBook to be Managed:")
+                if name in self.address_book_sytem:
+                    self.display_menu(name)
+                else:
+                    print(f"AddressBook with {name} not found")
+
             elif choice==3:
-                self.delete_contact_console()
+                name=input("Enter the name of AddressBook to be Deleted:")
+                self.del_addbook(name)
+
             elif choice==4:
-                self.address_book.view_all_contact()
+                for addbook in self.address_book_sytem:
+                    print(addbook)
             else:
-                print("Invalid choice:")
+                print("Invalid Choice")
                 break
 
-    def add_contact_console(self):  # add a new Contact
+    def check_add_addbook(self,name): # add multiple Address Book to the System
+        if name in self.address_book_sytem:
+            print(f"AddressBook with {name} already Exists")
+            return
+        else:
+            address_book=Addressbook()
+            self.address_book_sytem[name]=address_book
+            print("New AddressBook Created Successfully")
+
+    def del_addbook(self,name):
+        if name in self.address_book_sytem:
+            del self.address_book_sytem[name]
+            print(f"AddressBook {name} Deleted Successfully")
+            return
+        else:
+            print(f"AddressBook {name} not found")
+
+        
+    def display_menu(self,name):
+        if name in self.address_book_sytem:
+
+            while True:  # add multiple person
+                choice=int(input("1. Add Contact\n2. Edit Contact\n3. Delete Contact\n4. View Contacts\n5. Return to Main Menu\nEnter your choice:"))
+                if choice==1:
+                    self.add_contact_console(name,self.address_book_sytem[name])
+                elif choice==2:
+                    self.edit_contact_console(name,self.address_book_sytem[name])
+                elif choice==3:
+                    self.delete_contact_console(name,self.address_book_sytem[name])
+                elif choice==4:
+                    self.view_all_contact_console(self.address_book_sytem[name])
+                elif choice==5:
+                    break
+
+    def add_contact_console(self,addbook_name,addbook):  # add a new Contact
         print("Enter the details:")
         firstname=input("FirstName:")
         lastname=input("LastName:")
@@ -34,9 +78,8 @@ class AddressBookMain:
         email=input("E-mail:")
 
         new_contact=Contact(firstname,lastname,address,city,state,zip,phonenumber,email)
-        self.Ab.add_contact(new_contact)
 
-    def edit_contact_console(self):  # edit Contact
+    def edit_contact_console(self,addbook_name,addbook):  # edit Contact
         firstname = input("Enter the First name of Contact to be edited:")
         lastname=input("LastName:")
         address=input("Address:")
@@ -47,12 +90,18 @@ class AddressBookMain:
         email=input("E-mail:")
 
         updated_contact=Contact(firstname,lastname,address,city,state,zip,phonenumber,email)
-        self.Ab.edit_contact(firstname,updated_contact)        
 
-    def delete_contact_console(self):  # delete existing contact
+    def delete_contact_console(self,addbook_name,addbook):  # delete existing contact
         firstname = input("Enter the First name of Contact to be Deleted:")
-        self.Ab.delete_contact(firstname)
+        if isinstance(addbook,Addressbook):
+            addbook.delete_contact(firstname)
+        
+    def view_all_contact_console(self,add_obj):
+        if isinstance(add_obj,Addressbook):
+            add_obj.view_all_contact()
+
+    
 
 add1=AddressBookMain()
-add1.display_menu()
+add1.display_addressbook_menu()
 
